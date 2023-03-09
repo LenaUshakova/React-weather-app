befe07ad44b04765784b46d26316be2a8d847c9f
LenaUshakova <4elena.u@gmail.com>
2023-03-08 23:45:15 -0500

    Added Weather component, HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <footer className="footer">
-        <a
-          href="https://github.com/LenaUshakova/React-weather-app"
-          target={"_blank"}
-          rel="noreferrer"
-          className="footer-link"
-        >
-          Open-source code
-        </a>
-        {", "}by{" "}
-        <a
-          href="https://www.linkedin.com/in/lena-ushakova/"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-link"
-        >
-          Olena Ushakova
-        </a>
-      </footer>
+      <div className="content">
+        <Weather />
+        <footer className="footer">
+          <a
+            href="https://github.com/LenaUshakova/React-weather-app"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            Open-source code
+          </a>
+          {", "}by{" "}
+          <a
+            href="https://www.linkedin.com/in/lena-ushakova/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-link"
+          >
+            Olena Ushakova
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <div className="input-form">
+        <input
+          type="search"
+          placeholder="Enter a city"
+          className="search-field"
+        />
+        <i
+          className="fa-solid fa-location-crosshairs"
+          id="location-pointer"
+        ></i>
+      </div>
+      <div className="main-info">
+        <div className="city-time">
+          <div className="city">Kyiv</div>
+          <div className="weekday">Monday</div>
+          <div className="time">22:11</div>
+        </div>
+        <div className="weather-icon">
+          {" "}
+          <img
+            src="https://uxwing.com/wp-content/themes/uxwing/download/weather/cloudy-icon.png"
+            alt=""
+          />
+        </div>
+        <div className="current-weather">
+          <div className="temperature inline">
+            <span className="value">1</span>
+            <span className="unit">C</span>
+          </div>
+          <div className="weather-text">Cloudy</div>
+          <div className="min-temperature inline">10</div>
+          <div className="celsius inline">{"° "}/</div>
+          <div className="max-temperature inline">14</div>
+          <div className="celsius inline">°</div>
+        </div>
+        <div className="weather-detail">
+          <div className="precipitation">
+            <span className="detail">3</span>
+            <span className="detail"> mm</span>
+          </div>
+          <div className="wind">
+            <span className="detail">2</span>
+            <span className="detail"> km/h</span>
+          </div>
+          <div className="humidity">
+            <span className="detail">13</span>
+            <span className="detail"> %</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
