3fad27518d38e1b90848838c2cfbdd5b38594206
LenaUshakova <4elena.u@gmail.com>
2023-03-12 23:36:49 -0400

    Added search city function

---
+++ src/MainInfo.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function MainInfo(props) {
+  return (
+    <div className="main-info">
+      <div className="city-time">
+        <div className="city">{props.data.name}</div>
+        <FormattedDate date={props.data.date} />
+      </div>
+      <div className="weather-icon">
+        {" "}
+        <img src={props.data.icon} alt={props.data.description} />
+      </div>
+      <div className="current-weather">
+        <div className="temperature inline">
+          <span className="value">{props.data.temperature}</span>
+          <span className="unit">C</span>
+        </div>
+        <div className="weather-text">{props.data.description}</div>
+        <div className="min-temperature inline">{props.data.mintemp}</div>
+        <div className="celsius inline">{"° "}/ </div>
+        <div className="max-temperature inline">{props.data.maxtemp}</div>
+        <div className="celsius inline">°</div>
+      </div>
+      <div className="weather-detail">
+        <div className="precipitation">
+          <i className="fa-solid fa-cloud-rain weather-detail-icon"></i>
+          <span className="detail">{props.data.precipitation}</span>
+          <span className="detail"> mm</span>
+        </div>
+        <div className="wind">
+          <i className="fa-solid fa-wind weather-detail-icon"></i>
+          <span className="detail">{props.data.wind}</span>
+          <span className="detail"> km/h</span>
+        </div>
+        <div className="humidity">
+          <i className="fa-solid fa-droplet weather-detail-icon"></i>
+          <span className="detail">{props.data.humidity}</span>
+          <span className="detail"> %</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+import MainInfo from "./MainInfo";
 
 export default function Weather() {
   const [forecast, setForecast] = useState({ loaded: false });
-  const city = "Kyiv";
-
-  // const [city, setCity] = useState("Kyiv");
+  const [city, setCity] = useState("Kyiv");
 
   function showWeather(response) {
     console.log(response.data);
@@ -16,7 +14,9 @@ export default function Weather() {
       name: response.data.name,
       date: new Date(response.data.dt * 1000),
       temperature: Math.round(response.data.main.temp),
-      // precipitation: response.data.precipitation.value,
+      // precipitation: response.data.rain[0],
+      maxtemp: Math.round(response.data.main.temp_max),
+      mintemp: Math.round(response.data.main.temp_min),
       description: response.data.weather[0].main,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
@@ -25,65 +25,42 @@ export default function Weather() {
     });
   }
 
+  function searchCity(city) {
+    const apiKey = "50ee506356ef421435ae534c3e4094fb";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchCity(city);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   if (forecast.loaded) {
     return (
       <div className="Weather">
-        <div className="input-form">
+        <form className="input-form" onSubmit={handleSubmit}>
           <input
             type="search"
             placeholder="Enter a city"
             className="search-field"
             autoFocus="on"
+            onChange={updateCity}
           />
           <i
             className="fa-solid fa-location-crosshairs"
             id="location-pointer"
           ></i>
-        </div>
-        <div className="main-info">
-          <div className="city-time">
-            <div className="city">{forecast.name}</div>
-            <FormattedDate date={forecast.date} />
-          </div>
-          <div className="weather-icon">
-            {" "}
-            <img src={forecast.icon} alt={forecast.description} />
-          </div>
-          <div className="current-weather">
-            <div className="temperature inline">
-              <span className="value">{forecast.temperature}</span>
-              <span className="unit">C</span>
-            </div>
-            <div className="weather-text">{forecast.description}</div>
-            <div className="min-temperature inline">10</div>
-            <div className="celsius inline">{"° "}/ </div>
-            <div className="max-temperature inline">14</div>
-            <div className="celsius inline">°</div>
-          </div>
-          <div className="weather-detail">
-            <div className="precipitation">
-              <i className="fa-solid fa-cloud-rain weather-detail-icon"></i>
-              <span className="detail">{forecast.precipitation}</span>
-              <span className="detail"> mm</span>
-            </div>
-            <div className="wind">
-              <i className="fa-solid fa-wind weather-detail-icon"></i>
-              <span className="detail">{forecast.wind}</span>
-              <span className="detail"> km/h</span>
-            </div>
-            <div className="humidity">
-              <i className="fa-solid fa-droplet weather-detail-icon"></i>
-              <span className="detail">{forecast.humidity}</span>
-              <span className="detail"> %</span>
-            </div>
-          </div>
-        </div>
+        </form>
+        <MainInfo data={forecast} />
       </div>
     );
   } else {
-    const apiKey = "50ee506356ef421435ae534c3e4094fb";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(showWeather);
+    searchCity(city);
   }
 }
