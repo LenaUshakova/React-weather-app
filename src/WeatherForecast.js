3a478187945206095e0047a01930c2672354c3e7
LenaUshakova <4elena.u@gmail.com>
2023-03-21 00:04:57 -0400

    Added CSS to WeatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,54 @@ export default function WeatherForecast() {
           <div className="celsius inline">°</div>
         </div>
       </div>
+      <div className="col-md-2">
+        <div className="forecast-day">Mon</div>
+        <div className="forecast-icon">
+          <WeatherIcon code="03d" size={45} />
+        </div>
+        <div>
+          <div className="min-temperature inline">3</div>
+          <div className="celsius inline">° / </div>
+          <div className="max-temperature inline">8</div>
+          <div className="celsius inline">°</div>
+        </div>
+      </div>
+      <div className="col-md-2">
+        <div className="forecast-day">Mon</div>
+        <div className="forecast-icon">
+          <WeatherIcon code="03d" size={45} />
+        </div>
+        <div>
+          <div className="min-temperature inline">3</div>
+          <div className="celsius inline">° / </div>
+          <div className="max-temperature inline">8</div>
+          <div className="celsius inline">°</div>
+        </div>
+      </div>
+      <div className="col-md-2">
+        <div className="forecast-day">Mon</div>
+        <div className="forecast-icon">
+          <WeatherIcon code="03d" size={45} />
+        </div>
+        <div>
+          <div className="min-temperature inline">3</div>
+          <div className="celsius inline">° / </div>
+          <div className="max-temperature inline">8</div>
+          <div className="celsius inline">°</div>
+        </div>
+      </div>
+      <div className="col-md-2">
+        <div className="forecast-day">Mon</div>
+        <div className="forecast-icon">
+          <WeatherIcon code="03d" size={45} />
+        </div>
+        <div>
+          <div className="min-temperature inline">3</div>
+          <div className="celsius inline">° / </div>
+          <div className="max-temperature inline">8</div>
+          <div className="celsius inline">°</div>
+        </div>
+      </div>
     </div>
   );
 }
